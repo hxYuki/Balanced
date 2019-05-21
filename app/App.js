ecc5ffaae9e7bb9957d4abc787c9b49d19487914
hissyu <hissyu@hotmail.com>
2019-05-21 21:34:23 +0800

    Add new library & Homepage component

--- app/App.js
+++ app/App.js
@@ -9,6 +9,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+import Main from './conponents/MainPage/Main';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -20,11 +22,7 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Main/>
     );
   }
 }
