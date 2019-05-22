f84e9c901a1dc51a6936b2044d46f543c10a22f7
hissyu <hissyu@hotmail.com>
2019-05-22 11:52:48 +0800

    Make clear extension

--- app/extends/Number.js
+++ app/extends/Number.js
@@ -1,13 +1,20 @@
-const Extend1 = Number.prototype.formatCurrency = function (places, symbol, thousand, decimal) {
-    places = !isNaN(places = Math.abs(places)) ? places : 2;
+/**
+ * @param {Number} place place of decimal point
+ * @param {Char} symbol symbol for Currency
+ * @param {Char} thousand symbol for thousand delimiter
+ * @param {Char} decimal symbol for decimal point
+ * @returns {String} string in a currency formation
+ */
+Number.prototype.formatCurrency = function (place, symbol, thousand, decimal) {
+    place = !isNaN(place = Math.abs(place)) ? place : 2;
     symbol = symbol !== undefined ? symbol : "$";
     thousand = thousand || ",";
     decimal = decimal || ".";
     var number = this,
         negative = number < 0 ? "-" : "",
-        i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
+        i = parseInt(number = Math.abs(+number || 0).toFixed(place), 10) + "",
         j = (j = i.length) > 3 ? j % 3 : 0;
-    return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
+    return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (place ? decimal + Math.abs(number - i).toFixed(place).slice(2) : "");
 };
 
-export {Extend1};
+export default Number;
