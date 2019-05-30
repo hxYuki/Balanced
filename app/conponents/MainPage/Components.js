d9033f6031b40ec2117146c2dfa73d04fecb980a
hissyu <hissyu@hotmail.com>
2019-05-30 11:25:40 +0800

    Modified Mainpage look & fix a bug in database

--- app/conponents/MainPage/Components.js
+++ app/conponents/MainPage/Components.js
@@ -7,9 +7,18 @@ const ListHeader = ({income, expense, deposit}) => (
   <View style={ListHeaderStyle.Container}>
     <Text h4 style={ListHeaderStyle.ListTextCommon}>This week: {(income + expense).formatCurrency({symbol:'￥'})}</Text>
     <View style={ListHeaderStyle.ListTextContainer}>
-      <Text style={ListHeaderStyle.ListTextCommon}>Income: {income.formatCurrency({symbol:'￥'})}</Text>
-      <Text style={ListHeaderStyle.ListTextCommon}>Expense: {expense.formatCurrency({symbol:'￥'})}</Text>
-      <Text style={ListHeaderStyle.ListTextCommon}>Deposit: {deposit.formatCurrency({symbol:'￥'})}</Text>
+      <View style={ListHeaderStyle.ListInnerTextContainer}>
+        <Text style={ListHeaderStyle.ListTextCommon}>Income: </Text>
+        <Text style={ListHeaderStyle.ListTextStrong}>{income.formatCurrency({ symbol: '￥' })}</Text>
+      </View>
+      <View style={ListHeaderStyle.ListInnerTextContainer}>
+        <Text style={ListHeaderStyle.ListTextCommon}>Expense: </Text>
+        <Text style={ListHeaderStyle.ListTextStrong}>{expense.formatCurrency({symbol:'￥'})}</Text>
+      </View>
+      <View style={ListHeaderStyle.ListInnerTextContainer}>
+        <Text style={ListHeaderStyle.ListTextCommon}>Deposit: </Text>
+        <Text style={ListHeaderStyle.ListTextStrong}>{deposit.formatCurrency({symbol:'￥'})}</Text>
+      </View>
     </View>
   </View>
 );
@@ -25,12 +34,26 @@ const ListHeaderStyle = StyleSheet.create({
   },
   ListTextContainer:{
     flexDirection:'row',
-    justifyContent:'space-between'
+    justifyContent:'space-between',
+    flex:1
+  },
+  ListInnerTextContainer:{
+    // flexDirection:'row',
+    flex:1,
+    // alignItems:'center'
+    marginLeft:5,
+    marginRight:5
   },
   ListTextCommon:{
     color:ThemeConfig.themeTextColor,
-    fontSize:15,
-    flex:1
+    fontSize:12,
+    
+  },
+  ListTextStrong:{
+    color:ThemeConfig.themeTextColor,
+    fontSize:17,
+    textAlign:'right',
+    flex:1,
   }
 })
 
