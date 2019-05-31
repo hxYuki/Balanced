6c201e702a3e61ee50e2b50cf7f2ecf659f121d5
hissyu <hissyu@hotmail.com>
2019-05-31 19:35:59 +0800

    Make it right

--- app/App.js
+++ app/App.js
@@ -8,149 +8,12 @@
 
 import React, {Component} from 'react';
 import {AsyncStorage, ToastAndroid,View} from 'react-native';
+import moment from 'moment';
 import './extends/Number';
 
-import Main from './conponents/MainPage/Main';
+import Drawer from './components/Drawer/Drawer';
 import Sqlite from './lib/sqlite';
-import { DatabaseConfig, TableBasicAccounting, TableTags, DBVersion } from './config/DatabaseConfig';
-
-
-const checkUpdated = async () => {
-  let today = ((new Date()).toDateString('zh-Hans',{year:'numeric',month:'numeric',day:'numeric'}));
-  try {
-    let lastCheck = await AsyncStorage.getItem('today');
-    if(lastCheck){
-      if((new Date(today))>(new Date(lastCheck))){
-        // Need update
-        return 'outdated';
-      }
-    }
-  } catch (e) {
-    console.log('AsyncStorage Err: ',e);
-    return 'unchecked';
-  }
-  //
-  return 'updated';
-}
-const updateCycles = async () => {
-  try{
-    console.log('updating');
-    
-    let cycles = await db.in(TableBasicAccounting.name).where('cycleCount is not null AND nextTriggerTime is not null').select();
-    console.log(cycles);
-    
-    // let cyclesNeedUpdate = cycles.filter()
-  }catch(e){
-
-  }
-}
-const popinSomeData = async () => {
-  const data = [
-    {
-      amount:10,
-      note:'',
-      method:0,
-      usage:0,
-      cycleCount:1,
-      cycleUnit:0,
-      firstTime: new Date()
-    },
-    {
-      amount:25,
-      note:'',
-      method:0,
-      usage:0,
-      firstTime: (new Date()).toString()
-    },
-    {
-      amount:100,
-      note:'',
-      method:0,
-      usage:0,
-      firstTime: (new Date()).toString()
-    }
-  ];
-  await db.in('BaseTable').insert(data);
-  console.log('data ok');
-  
-}
-const clearStorage = async () => {
-  try {
-    AsyncStorage.clear();
-  } catch (e) {
-    
-  }
-}
-const initDB = async () => {
-  try {
-    await db.createTable(TableBasicAccounting);
-    await db.createTable(TableTags);
-    console.log('init ed');
-  } catch (e) {
-    console.log('Init err: ',e);
-    return false;
-  }
-  
-}
-const checkDatabase = async () => {
-  try{
-    console.log('checking');
-    
-    let dbVersion = await AsyncStorage.getItem('dbVersion');
-    if(!dbVersion){
-      ToastAndroid.show('Initializing...',ToastAndroid.LONG);
-      console.log('initializing');
-      
-      await initDB();
-      AsyncStorage.setItem('dbVersion',DBVersion);
-    }
-    // DB exists
-    return 'DB OK';
-  }catch(e){
-    console.log('Exception happens: ',e);
-    return 'unchecked';
-  }
-  return 'NO DB';
-}
-const clearDB = async () => {
-  try{
-    AsyncStorage.removeItem('dbVersion');
-    await db.dropTable(TableBasicAccounting.name);
-    await db.dropTable(TableTags.name);
-    console.log('clear!');
-  }catch(e){
-    console.log(e);
-  }
-  
-}
-
-const checkThings = async ()=>{
-  return checkDatabase().then(res=>{
-    switch (res) {
-      case 'DB OK':
-        return true;
-      case 'NO DB':
-        ToastAndroid.show('DB init Failed',ToastAndroid.SHORT);
-      case 'unchecked':
-        ToastAndroid.show('DB Check Failed.',ToastAndroid.SHORT);
-    }
-    return false;
-  }).then(res=>{
-    if(res)
-      return checkUpdated();
-  }).then(res=>{
-    switch(res){
-      case 'outdated':
-        //TODO: update
-        updateCycles();
-        break;
-      case 'updated':
-        break;
-      case 'unchecked':
-        ToastAndroid.show('Up-to-date Check Failed.',ToastAndroid.SHORT);
-    }
-  });
-}
+import { DatabaseConfig, TableBasicAccounting, TableTags, DBVersion, BaseTableFieldTitle } from './config/DatabaseConfig';
 
 var db= new Sqlite(DatabaseConfig);
 type Props = {};
@@ -161,10 +24,14 @@ export default class App extends Component<Props> {
     // console.log(DatabaseConfig,{name:'Balanced'});
     
     // checkThings().then(()=>{
-      // popinSomeData();
+    //   // popinSomeData();
     // })
     // clearStorage();
-    // clearDB();
+    // clearDB().then(()=>{
+    //   return checkThings();
+    // }).then(()=>{
+    //   popinSomeData();
+    // })
     // updateCycles();
   }
   componentDidMount(){
@@ -173,10 +40,7 @@ export default class App extends Component<Props> {
   }
   render() {
     return (// TODO : Replace this with Drawer component
-      <View>
-        <Main openDrawer={()=>{}} db={db} />
-
-      </View>
+      <Drawer db={db} />
     );
   }
 }
--- app/components/MainPage/Main.js
+++ app/components/MainPage/Main.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, SectionList, StatusBar, StyleSheet, ToastAndroid} from 'react-native';
+import {View, Text, SectionList, StatusBar, StyleSheet, ToastAndroid, Dimensions} from 'react-native';
 import {Header, ListItem} from 'react-native-elements';
 import moment from 'moment';
 import {List} from 'immutable';
@@ -11,17 +11,174 @@ import {TableBasicAccounting, BaseTableFieldTitle} from '../../config/DatabaseCo
 import Floatwindow from '../FloatWindow/FloatWindow';
 
 
+const height = Dimensions.get('window').height;
 
-
-const parseMonth=(n) => {return (moment(n).format('MMMM'));}
+const parseYearMonth=(n) => {return (moment(n).format('YYYY-MM'));}
 const countListData = (list: List) => (list.reduce((acc,v)=>(acc+v.data.length),0))
 
 type OpenDrawerCallback = ()=>{}
 type SetTotalDepositCallback = (totalDeposit:Number)=>{}
 type Props = {
-  openDrawer: OpenDrawerCallback,
-  setTotalDeposit?: SetTotalDepositCallback,
-  db?: Sqlite
+  openDrawerCB: OpenDrawerCallback,
+  setTotalDeposit: SetTotalDepositCallback,
+  db: Sqlite
+}
+
+const checkUpdated = async () => {
+  let today = ((new Date()).toDateString('zh-Hans',{year:'numeric',month:'numeric',day:'numeric'}));
+  try {
+    let lastCheck = await AsyncStorage.getItem('today');
+    if(lastCheck){
+      if((new Date(today))>(new Date(lastCheck))){
+        // Need update
+        return 'outdated';
+      }
+    }
+  } catch (e) {
+    console.log('AsyncStorage Err: ',e);
+    return 'unchecked';
+  }
+  //
+  return 'updated';
+}
+const updateCycles = async () => {
+  // const map = {
+  //   'Year':'y',
+  //   'Month':'m',
+  //   ''
+  // }
+  try{
+    console.log('updating');
+    
+    let cycles = await db.in(TableBasicAccounting.name).field('*').where('cycleCount is not null AND nextTriggerTime==date("now")').select();
+    // console.log('toUpdate',cycles);
+    
+    cycles.forEach(v=>{
+      let d = {...v,nextTriggerTime:null,firstTime:v.nextTriggerTime,cycleCount:null};
+      delete d.id;
+      // console.log('cycle',d);
+      db.in(TableBasicAccounting.name).insert(d);
+      db.in(TableBasicAccounting.name).where(`id==${v.id}`).update({'nextTriggerTime':moment(v.nextTriggerTime).add(v.cycleCount,BaseTableFieldTitle.cycleUnit[v.cycleUnit]).format('YYYY-MM-DD')});
+      // console.log('next',moment(v.nextTriggerTime).add('W',v.cycleCount).format('YYYY-MM-DD'));
+      
+    })
+    // let cyclesNeedUpdate = cycles.filter()
+  }catch(e){
+
+  }
+}
+const popinSomeData = async () => {
+  const data = [
+    {
+      amount:10,
+      note:'',
+      method:0,
+      usage:0,
+      cycleCount:1,
+      cycleUnit:0,
+      firstTime: moment().format('YYYY-MM-DD')
+    },{
+      amount:25,
+      note:'',
+      method:0,
+      usage:0,
+      firstTime: moment().format('YYYY-MM-DD')
+    },{
+      amount:100,
+      note:'',
+      method:0,
+      usage:0,
+      firstTime: moment().format('YYYY-MM-DD')
+    },{
+      amount:-40,
+      note:'',
+      method:0,
+      usage:0,
+      firstTime: moment().add(-1,'y').format('YYYY-MM-DD'),
+      nextTriggerTime: moment().format('YYYY-MM-DD'),
+      cycleCount: 10
+    }
+  ];
+  await db.in(TableBasicAccounting.name).insert(data);
+  console.log('data ok');
+  
+}
+const clearStorage = async () => {
+  try {
+    AsyncStorage.clear();
+  } catch (e) {
+    
+  }
+}
+const initDB = async () => {
+  try {
+    await db.createTable(TableBasicAccounting);
+    await db.createTable(TableTags);
+    console.log('init ed');
+  } catch (e) {
+    console.log('Init err: ',e);
+    return false;
+  }
+  
+}
+const checkDatabase = async () => {
+  try{
+    console.log('checking');
+    
+    let dbVersion = await AsyncStorage.getItem('dbVersion');
+    if(!dbVersion){
+      ToastAndroid.show('Initializing...',ToastAndroid.LONG);
+      console.log('initializing');
+      
+      await initDB();
+      AsyncStorage.setItem('dbVersion',DBVersion);
+    }
+    // DB exists
+    return 'DB OK';
+  }catch(e){
+    console.log('Exception happens: ',e);
+    return 'unchecked';
+  }
+  return 'NO DB';
+}
+const clearDB = async () => {
+  try{
+    AsyncStorage.removeItem('dbVersion');
+    await db.dropTable(TableBasicAccounting.name);
+    await db.dropTable(TableTags.name);
+    console.log('clear!');
+  }catch(e){
+    console.log(e);
+  }
+  
+}
+
+const checkThings = async ()=>{
+  return checkDatabase().then(res=>{
+    switch (res) {
+      case 'DB OK':
+        return true;
+      case 'NO DB':
+        ToastAndroid.show('DB init Failed',ToastAndroid.SHORT);
+      case 'unchecked':
+        ToastAndroid.show('DB Check Failed.',ToastAndroid.SHORT);
+    }
+    return false;
+  }).then(res=>{
+    if(res)
+      return checkUpdated();
+  }).then(res=>{
+    switch(res){
+      case 'outdated':
+        //TODO: update
+        return updateCycles();
+        break;
+      case 'updated':
+        break;
+      case 'unchecked':
+        ToastAndroid.show('Up-to-date Check Failed.',ToastAndroid.SHORT);
+    }
+  });
 }
 
 var db:Sqlite;
@@ -36,19 +193,22 @@ export default class Main extends Component<Props>{
     };
     db=this.props.db;
 
+    checkThings().then(()=>{
+      this.refreshAfterSubmitted();
+    })
   }
   componentDidMount(){
     this.queryListData();
     this.queryStatData();
   }
-  async processSections(initialData:Array) {
+  processSections(initialData:Array) {
     if (initialData.length === 0) return;
     let t = {
       title: '',
       data: []
     };
-    t.title = parseMonth(initialData[0]['firstTime']);
-    t.data = initialData.filter(v => t.title === parseMonth(v['firstTime']));
+    t.title = parseYearMonth(initialData[0]['firstTime']);
+    t.data = initialData.filter(v => t.title === parseYearMonth(v['firstTime']));
     let hasIndex = -1;
     if (-1 !== (hasIndex = this.state.accounts.findIndex(v => v.title === t.title))) {
       let newT = this.state.accounts.get(hasIndex);
@@ -62,16 +222,17 @@ export default class Main extends Component<Props>{
       });
     }
     
-    return this.processSections(initialData.filter(v => t.title !== parseMonth(v['firstTime'])));
+    return this.processSections(initialData.filter(v => t.title !== parseYearMonth(v['firstTime'])));
   }
   async queryListData(){
     console.log('querying');
     
-    let results = await db.in(TableBasicAccounting.name).limit(10,countListData(this.state.accounts)).orderedBy('firstTime','id','desc').select();
-
+    let results = await db.in(TableBasicAccounting.name).limit(10,countListData(this.state.accounts)).orderedBy('id','desc').select();
+    // console.log(results);
+    
     if(!results)
     {
-      ToastAndroid.show('Failed to connect to DB!');
+      ToastAndroid.show('No more records!',ToastAndroid.SHORT);
       return;
     }
     this.processSections(results);
@@ -94,41 +255,45 @@ export default class Main extends Component<Props>{
       .groupBy('month')
       .where('month=strftime("%m","now") and (usage==6 or method==4)')
       .select();
-    
-    // let r= await db.execRaw(`select date('')`)
-    console.log(deposit);
-    
+     
     this.setState({
       income:income?income[0]['total']:0,
       expense:expense?expense[0]['total']:0,
       deposit:deposit?deposit[0]['total']:0
     })
-    
-    if(this.props.setTotalDeposit!==undefined){
+
+    {
       let totalDeposit = await db.in(TableBasicAccounting.name)
-      .field("method, usage, sum(amount) as total")
-      .where('usage==6 or method==4')
-      .select();
-      this.props.setTotalDeposit(totalDeposit?totalDeposit[0]['total']:0)
+        .field("method, usage, sum(amount) as total")
+        .where('(usage==6 or method==4)')
+        .select();
+        
+      this.props.setTotalDeposit(parseInt(totalDeposit[0]['total']).formatCurrency({symbol:''}))
     }
     
   }
+  async refreshAfterSubmitted(){
+    await this.queryListData();
+    await this.queryStatData();
+    this.refs['section'].props.refreshing=false
+  }
   render(){
     return (
-      <View>
+      <View style={{height:height}}>
         <StatusBar translucent barStyle={'light-content'} backgroundColor={'rgba(0, 0, 0, 0.3)'} />
         <Header
           backgroundColor={ThemeConfig.themeMainColor}
           containerStyle={{borderBottomWidth:0}}
           placement="left"
-          leftComponent={{ icon: "menu", color: "#fff" , onPress:this.props.openDrawer()}}
+          leftComponent={{ icon: "menu", color: "#fff" , onPress:()=>{this.props.openDrawerCB()}}}
           centerComponent={{ text: "Balanced", style: { color: "#fff" } }}
           // statusBarProps={{ translucent: true, barStyle: "light-content" }}
         />
         <SectionList
           sections={this.state.accounts.toJS()}
+          ref={'section'}
           stickySectionHeadersEnabled
-          renderSectionHeader={({section:{title}})=><Text style={MainStyle.SectionHeaderStyle}>{title}</Text>}
+          renderSectionHeader={({section:{title}})=><Text style={MainStyle.SectionHeaderStyle}>{moment(title).format('MMMM')}</Text>}
           ListHeaderComponent={<ListHeader income={this.state.income} expense={this.state.expense} deposit={this.state.deposit} />}
           renderItem={({ item }) => 
             (<ListItem
@@ -143,10 +308,20 @@ export default class Main extends Component<Props>{
             />)}
           keyExtractor={(item,index)=>index.toString()}
           // onEndReachedThreshold={}
-          // onEndReached={()=>{this.queryListData()}}
+          onEndReached={()=>{this.queryListData()}}
+          refreshing={false}
+          onRefresh={()=>{
+            this.refs['section'].props.refreshing=true;
+            this.setState({
+              accounts: this.state.accounts.clear(),
+              income: 0,
+              expense: 0,
+              deposit: 0
+            }, ()=>{this.refreshAfterSubmitted()});
+          }}
         />
 
-        {/* <Floatwindow /> */}
+        {/* <Floatwindow db={db} refresh /> */}
       </View>
     );
   }
